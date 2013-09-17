92d991ec2f65a58fe9d5d4d0d2c16b058cda2da9
Felipe Garcia Lopez <flg@flg-ThinkPad-W530.(none)>
2013-09-17 10:27:21 +0200

    tf broadcaster implemented

---
+++ src/publish_coordinate_systems.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import roslib;
+import rospy
+import tf
+import math
+
+from tf.msg import tfMessage
+from std_msgs.msg import String 
+from geometry_msgs.msg import PoseStamped
+
+def publish_coordinate_systems():
+    
+    rospy.init_node('publish_coordinate_systems')
+    
+    # start transform boradcaster
+    br = tf.TransformBroadcaster()
+
+    # Pose Stamped publishing ##############################################
+    #pub = rospy.Publisher('PoseBaseToCamera', PoseStamped)
+    #p = PoseStamped()
+    
+    #quaternions = tf.transformations.quaternion_from_euler(-0.78,-0.78,0)
+
+    #p.header.frame_id = "base_link"
+
+    #p.pose.position.x = 0
+    #p.pose.position.y = 0.395
+    #p.pose.position.z = 0.49
+
+    #p.pose.orientation.x = quaternions[0]
+    #p.pose.orientation.y = quaternions[1]
+    #p.pose.orientation.z = quaternions[2]
+    #p.pose.orientation.w = quaternions[3]
+    #######################################################################
+
+    while not rospy.is_shutdown():
+        # describing "/stereo/left" wrt "base_link"
+        br.sendTransform((0, 0.395, 0.49),
+                     tf.transformations.quaternion_from_euler(-0.78, -0.78, 0),
+                     rospy.Time.now(),
+                     "/stereo/left",
+                     "/base_link")
+        # describing "/hanle_link" wrt "base_link"
+        br.sendTransform((0.2, 0.0, 0.0),
+                     tf.transformations.quaternion_from_euler(0, 0, 0),
+                     rospy.Time.now(),
+                     "/handle_link",
+                     "/base_link")
+        ###################################################################
+        #pub.publish(p)
+        ###################################################################
+        rospy.sleep(1.0)
+
+if __name__ == '__main__':
+    try:
+        publish_coordinate_systems()
+    except rospy.ROSInterruptException:
+        pass
